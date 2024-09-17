9b50b5e9da2e697092f9e521395a3f26e151490b
GentleHe <13789127308@163.com>
2024-09-17 22:47:08 +0800

    添加路由守卫

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeContainerComponent} from "./home";
 import {NoSiderContainerComponent} from "./main-data/components/no-sider-container/no-sider-container.component";
 import {LoginComponent} from "./account";
+import {loginGuardGuard} from "./login-guard.guard";
 
 const routes: Routes = [
   {path:'', component: HomeContainerComponent, loadChildren: () => import('./home').then(m => m.HomeModule)},
-  {path:'system', component:HomeContainerComponent, loadChildren: ()=>import('./system').then((m => m.SystemModule))},
+  {path:'system', component:HomeContainerComponent, loadChildren: ()=>import('./system').then((m => m.SystemModule)), canActivate: [loginGuardGuard]},
   {path:'shared', component:HomeContainerComponent, loadChildren: ()=>import('./shared').then((m => m.SharedModule))},
   {path:'main-data', component:HomeContainerComponent, loadChildren: ()=>import('./main-data').then((m => m.MainDataModule))},
   {path:'audit', component:HomeContainerComponent, loadChildren: ()=>import('./audit').then((m => m.AuditModule))},
